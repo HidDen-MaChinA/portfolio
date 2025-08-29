22edc3500cd72f31881a7ef581d105012f6b2b91
HidDen-MaChinA <hei.franco.3@gmail.com>
2025-08-29 14:03:05 +0200

    refactor!: remove /portfolio and make the main portfolio part easy to find

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import AboutMeSection from "@/components/app/portfolio/AboutMeSection";
+import AchievementsSection from "@/components/app/portfolio/AchievementsSection/AchievementsSection";
+import Footer from "@/components/app/portfolio/Footer/Footer";
+import HeroSection from "@/components/app/portfolio/HeroSection";
+import TopBar from "@/components/app/portfolio/Navigation/TopBar";
+import ProjectsSection from "@/components/app/portfolio/ProjectsSection/ProjectsSection";
+import SkillsSection from "@/components/app/portfolio/SkillsSection/SkillsSection";
 
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-    </div>
-  );
+export default function Portfolio(){
+    return (
+        <div>
+            <TopBar />
+            <HeroSection />
+            <div className="flex flex-col gap-[90px]">
+                <AboutMeSection />
+                <SkillsSection />
+                <ProjectsSection />
+                <AchievementsSection />
+            </div>
+            <Footer />
+        </div>
+    )
 }
